228cbbd39862d45a40c27493ef3da000c48393ca
Baptiste <baptiste.rebillard@outlook.fr>
2021-03-11 20:33:49 +0100

    2

--- index.php
+++ index.php
@@ -5,19 +5,16 @@
 			<meta charset="UTF-8">
 				<link rel="stylesheet" type="text/css" href="config/style.css" />
 	</head>
-	<script>
-	function draw() {
-  	//taille 290 * 140
-	  	var ctx = document.getElementById('canvas').getContext('2d');
-	}
-	</script>
-<body onload="draw();" bgcolor="black">
-	<canvas id="canvas"></canvas>
-	<div id="rotamiddle">
-		<img src="img/middle.png" height="100%" class="rota">
-	</div>
+		<body bgcolor="black">
+			<!--<audio src="img/gen.mp3" controls></audio>-->
 
-	<img src="img/1.png" class="png1">
-	<img src="img/2.png" class="png2">
-</body>
+			<div id="rotamiddle">
+				<img src="img/middle.png" height="100%" class="rota">
+			</div>
+
+			<img src="img/1.png" class="png1">
+			<img src="img/2.png" class="png2">
+
+			<img src="img/nyancat.gif" class="nyan">
+		</body>
 </html>
