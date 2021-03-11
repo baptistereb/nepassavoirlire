04e87e2754c995bcb2caa75564a48af3346d9de1
Baptiste <baptiste.rebillard@outlook.fr>
2021-03-11 19:56:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NEPASSAVOIRLIRE.XYZ</title>
+			<meta charset="UTF-8">
+				<link rel="stylesheet" type="text/css" href="config/style.css" />
+	</head>
+	<script>
+	function draw() {
+  	//taille 290 * 140
+	  	var ctx = document.getElementById('canvas').getContext('2d');
+	}
+	</script>
+<body onload="draw();">
+	<canvas id="canvas"></canvas>
+	<div id="rotamiddle">
+		<img src="img/middle.png" height="100%" class="rota">
+	</div>
+
+	<img src="img/1.png" class="png1">
+	<img src="img/2.png" class="png2">
+</body>
+</html>
