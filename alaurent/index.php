7ed6a8c1e171dcdf422c11aec44e111fc1b9c75c
Baptiste <baptiste.rebillard@outlook.fr>
2021-03-13 20:28:48 +0100

    3

---
+++ alaurent/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NEPASSAVOIRLIRE.XYZ</title>
+			<meta charset="UTF-8">
+				<link rel="stylesheet" type="text/css" href="config/style.css" />
+	</head>
+		<body bgcolor="black">
+			<!--<audio src="img/gen.mp3" controls></audio>-->
+
+			<div id="rotamiddle">
+				<img src="img/middle.png" height="100%" class="rota">
+			</div>
+
+			<img src="img/1.png" class="png1">
+			<img src="img/2.png" class="png2">
+
+			
+			<img src="img/cat.png" class="pngb">
+
+			<img src="img/man.png" class="rota2">
+			<img src="img/man.png" class="rota2">
+			<img src="img/man.png" class="rota2">
+			<img src="img/man.png" class="rota2">
+			<img src="img/man.png" class="rota2">
+
+			<img src="img/baby.png" class="pngb2">
+
+			<img src="img/smart.png" class="upsidedown">
+
+			<img src="img/nyancat.gif" class="nyan">
+		</body>
+</html>
